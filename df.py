5b050391cceb52ff2ca4df87bfc25dd21f647cce
Lucas <encarvlucas@hotmail.com>
2017-08-15 16:46:40 -0300

    -Separated permanent solution from time step solution (explicit/implicit) -Removed T_0 and T_L arguments form implicit solution -Preliminary implicit solution: working

--- df.py
+++ df.py
@@ -8,7 +8,7 @@ import os
 import math
 import matplotlib.pyplot as plt
 
-def diferencasfinitasimplicito(X ,MRE ,k ,Q ,Delta_t ,T_inicial ,T_0 ,T_L ,dT_0 ,dT_L):
+def diferencasfinitaspermanente(X,MRE,k,Q,T_0,T_L,dT_0,dT_L):
 	#MATRIZ DE SOLUÇÃO
 	L = max(X)
 	numele = len(X)-1 #NÚMERO DE ESPAÇOS DO DOMÍNIO
@@ -52,7 +52,49 @@ def diferencasfinitasimplicito(X ,MRE ,k ,Q ,Delta_t ,T_inicial ,T_0 ,T_L ,dT_0 
 	print resp
 	return resp
 
-def diferencasfinitasexplicito(X, MRE, k, Q, Delta_t, T_inicial, T_0, T_L, dT_0, dT_L):
+def diferencasfinitasimplicito(X ,MRE ,k ,Q ,Delta_t ,T_inicial ,dT_0 ,dT_L):
+	#MATRIZ DE SOLUÇÃO
+	L = max(X)
+	numele = len(X)-1 #NÚMERO DE ESPAÇOS DO DOMÍNIO
+	numpnts = numele + 1 #NÚMERO DE PONTOS PARA SEREM CALCULADOS OS VALORES DA FUNÇÃO
+	#----------------------MATRIZ GERAL (-K+M)*a = f--------------------------------------------------------------------
+	matriz = np.zeros((numpnts,numpnts))
+	matrizf = Q
+	for elem in MRE:
+		matriz[elem[0]][elem[0]] -= k*1.0/(X[elem[0]]-X[elem[1]])**2		#\
+		matriz[elem[1]][elem[0]] -= -k*1.0/(X[elem[0]]-X[elem[1]])**2		# \  MATRIZ	    [ -2  1  0 ...]
+		matriz[elem[0]][elem[1]] -= -k*1.0/(X[elem[0]]-X[elem[1]])**2		# /        =  k [  1 -2  1 ...]
+		matriz[elem[1]][elem[1]] -= k*1.0/(X[elem[0]]-X[elem[1]])**2		#/				[  0  1 -2 ...]
+
+	# T(0) = T_0 (CONDIÇÃO DE CONTORNO DE DIRICHLET)
+	index = np.where(X==0)[0][0]
+	matrizf[index] = T_inicial[0] 						# VALOR NO PONTO
+	matriz[index][index] = 1 							# ÚNICO COEFICIENTE DA LINHA E COLUNA
+	a = range(numpnts)
+	a.remove(index)
+	for i in a:
+		matrizf[i] -= matriz[i][index]*matrizf[index]	# SUBTRAÇÂO DA COMPONENTE DA LINHA
+		matriz[index][i] = 0 							#{	ELIMINAÇÃO DA LINHA
+		matriz[i][index] = 0 							#{	ELIMINAÇÃO DA COLUNA
+
+	# T(L) = T_L (CONDIÇÃO DE CONTORNO DE DIRICHLET)
+	index = np.where(X==L)[0][0]
+	matrizf[index] = T_inicial[-1] 						# VALOR NO PONTO
+	matriz[index][index] = 1 							# ÚNICO COEFICIENTE DA LINHA E COLUNA
+	a = range(numpnts)
+	a.remove(index)
+	for i in a:
+		matrizf[i] -= matriz[i][index]*matrizf[index]	# SUBTRAÇÂO DA COMPONENTE DA LINHA
+		matriz[index][i] = 0 							#{	ELIMINAÇÃO DA LINHA
+		matriz[i][index] = 0 							#{	ELIMINAÇÃO DA COLUNA
+
+	print matriz
+	print matrizf
+	resp = np.linalg.solve(matriz,matrizf)
+	print resp
+	return resp
+
+def diferencasfinitasexplicito(X, MRE, k, Q, Delta_t, T_inicial, dT_0, dT_L):
 	L = max(X)
 	numpnts = len(X) #NÚMERO DE PONTOS PARA SEREM CALCULADOS OS VALORES DA FUNÇÃO
 	resp = np.zeros(numpnts)
@@ -102,13 +144,16 @@ def main():
 	print "Q: ",Q
 
 	t_acumulado = 0.0
-	for i in range(len(t_intervalos)):
-		t_acumulado += t_intervalos[i]
-		T_inicial = diferencasfinitasexplicito(X, MRE, 1.0, Q, t_intervalos[i], T_inicial, T_inicial[0], T_inicial[-1], None, None)
-		grafico(X, T_inicial, (1-i*1.0/len(t_intervalos),0,0), "Metodo Explicito t={}".format(t_acumulado))
-		# plt.savefig("img/explicito_{}.jpg".format(i))
-		# plt.show()
-	plt.savefig("img/explicito_t={}s.jpg".format(t_acumulado))
+	# for i in range(len(t_intervalos)):
+	# 	t_acumulado += t_intervalos[i]
+	# 	T_inicial = diferencasfinitasexplicito(X, MRE, 1.0, Q, t_intervalos[i], T_inicial, None, None)
+	# 	grafico(X, T_inicial, (1-i*1.0/len(t_intervalos),0,0), "Metodo Explicito t={}".format(t_acumulado))
+	# 	# plt.savefig("img/explicito_{}.jpg".format(i))
+	# 	# plt.show()
+	# plt.savefig("img/explicito_t={}s.jpg".format(t_acumulado))
+	
+	T_inicial = diferencasfinitasimplicito(X, MRE, 1.0, Q, t_intervalos, T_inicial, None, None)
+	grafico(X, T_inicial, "b", "Metodo Teste")
 	plt.show()
 	return
 
