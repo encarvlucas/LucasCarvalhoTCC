7118794043bc74d19aa8c4f781969d0fb448f113
Lucas <encarvlucas@hotmail.com>
2018-09-13 19:00:42 -0300

    -Continuation of migration of script -Solution still not working

--- TccLib.py
+++ TccLib.py
@@ -15,7 +15,7 @@ def check_method_call(*args):
     """
     for arg in args:
         try:
-            if len(arg) and all(item for item in arg):
+            if len(arg):
                 return
         except TypeError:
             if arg:
@@ -103,11 +103,12 @@ def solve(mesh, permanent_solution=True):
         raise ValueError("The mesh is empty. Try using import_point_structure() before solving.")
 
     try:
-        if not (len(mesh.boundary_conditions.point_index_vector) and
-                len(mesh.boundary_conditions.values_vector) and
-                len(mesh.boundary_conditions.type_of_condition_vector)):
-            raise ValueError("There are no boundary conditions defined. "
-                             "Try using mesh.boundary_conditions.set_new_boundary_conditions() before solving.")
+        for boundary_condition in [mesh.space_boundary_conditions, mesh.time_boundary_conditions]:
+            if not (len(boundary_condition.point_index_vector) and
+                    len(boundary_condition.values_vector) and
+                    len(boundary_condition.type_of_condition_vector)):
+                raise ValueError("There are no boundary conditions defined. "
+                                 "Try using mesh.boundary_condition.set_new_boundary_conditions() before solving.")
     except TypeError:
         raise ValueError("There are no boundary conditions defined. "
                          "Try using mesh.boundary_conditions.set_new_boundary_conditions() before solving.")
@@ -115,11 +116,16 @@ def solve(mesh, permanent_solution=True):
     k_coef_x = 1.0
     k_coef_y = 1.0
     thickness = 1.0
+    dt = 0.1
+    total_time = 10.0
 
-    if permanent_solution:
-        # --- Defining the Matrices--------------------------------------------------------------------------------
-        q_matrix = sparse.lil_matrix((mesh.size, 1))  # Heat generation
-        k_matrix = sparse.lil_matrix((mesh.size, mesh.size))  # Stiffness matrix
+    def get_matrix():
+        """
+        Function that generates the algebraic components of the solution method
+        :return:
+        """
+        k_sparse = sparse.lil_matrix((mesh.size, mesh.size))  # Stiffness matrix
+        m_sparse = sparse.lil_matrix((mesh.size, mesh.size))  # Mass matrix
 
         for elem in mesh.ien:
             x = mesh.x[elem]
@@ -127,7 +133,7 @@ def solve(mesh, permanent_solution=True):
 
             a = ((x[0] * y[1] - x[1] * y[0]) +
                  (x[1] * y[2] - x[2] * y[1]) +
-                 (x[2] * y[0] - x[0] * y[2]))
+                 (x[2] * y[0] - x[0] * y[2])) / 2.0
 
             b = np.array([y[1] - y[2],
                           y[2] - y[0],
@@ -137,24 +143,40 @@ def solve(mesh, permanent_solution=True):
                           x[0] - x[2],
                           x[1] - x[0]])
 
-            k = -(thickness / (4 * a)) * (k_coef_x * np.array([[b[0] ** 2, b[0] * b[1], b[0] * b[2]],
-                                                               [b[0] * b[1], b[1] ** 2, b[1] * b[2]],
-                                                               [b[0] * b[2], b[1] * b[2], b[2] ** 2]]) +
-                                          k_coef_y * np.array([[c[0] ** 2, c[0] * c[1], c[0] * c[2]],
-                                                               [c[0] * c[1], c[1] ** 2, c[1] * c[2]],
-                                                               [c[0] * c[2], c[1] * c[2], c[2] ** 2]]))
+            k = -(thickness / (4.0 * a)) * (k_coef_x * np.array([
+                                                                 [b[0] * b[0], b[0] * b[1], b[0] * b[2]],
+                                                                 [b[0] * b[1], b[1] * b[1], b[1] * b[2]],
+                                                                 [b[0] * b[2], b[1] * b[2], b[2] * b[2]]
+                                                                ]) +
+                                            k_coef_y * np.array([
+                                                                 [c[0] * c[0], c[0] * c[1], c[0] * c[2]],
+                                                                 [c[0] * c[1], c[1] * c[1], c[1] * c[2]],
+                                                                 [c[0] * c[2], c[1] * c[2], c[2] * c[2]]
+                                                                ]))
+            if permanent_solution:
+                m = (a / 12.0) * np.array([[2, 1, 1], [1, 2, 1], [1, 1, 2]])
 
             for i in range(3):  # Used so because of the triangular elements
                 for j in range(3):
-                    k_matrix[elem[i], elem[j]] += k[i][j]
+                    k_sparse[elem[i], elem[j]] += k[i][j]
+                    if permanent_solution:
+                        m_sparse[elem[i], elem[j]] += m[i][j]
+
+        return k_sparse, m_sparse
+
+    if permanent_solution:
+
+        # --- Defining the Matrices-------------------------------------------------------------------------------------
+        q_matrix = sparse.lil_matrix((mesh.size, 1))  # Heat generation
+        k_matrix, m_matrix = get_matrix()  # Stiffness and Mass matrices
 
         # --------------------------------- Boundary conditions treatment ----------------------------------------------
         k_matrix = k_matrix.tocsc()
-        for index, point_index in enumerate(mesh.boundary_conditions.point_index_vector):
+        for index, point_index in enumerate(mesh.space_boundary_conditions.point_index_vector):
             for i in k_matrix[:, point_index].indices:
-                if mesh.boundary_conditions.type_of_condition_vector[index]:
+                if mesh.space_boundary_conditions.type_of_condition_vector[index]:
                     # Dirichlet Treatment
-                    q_matrix[i, 0] -= k_matrix[i, point_index] * mesh.boundary_conditions.values_vector[index]
+                    q_matrix[i, 0] -= k_matrix[i, point_index] * mesh.space_boundary_conditions.values_vector[index]
                 k_matrix[i, point_index] = 0
                 k_matrix[point_index, i] = 0
             k_matrix[point_index, point_index] = 1
@@ -162,6 +184,37 @@ def solve(mesh, permanent_solution=True):
 
         # --------------------------------- Solver ---------------------------------------------------------------------
         return linalg.spsolve(k_matrix, q_matrix)
+
+    else:
+        # --- Defining the Matrices-------------------------------------------------------------------------------------
+        q_matrix = sparse.lil_matrix((mesh.size, 1))  # Heat generation
+        k_matrix, m_matrix = get_matrix()  # Stiffness and Mass matrices
+
+        a_matrix = m_matrix - k_matrix * dt
+
+        # --------------------------------- Boundary conditions treatment ----------------------------------------------
+        def cc1(vec):
+            for i in mesh.space_boundary_conditions.point_index_vector:
+                vec[i] = mesh.time_boundary_conditions.values_vector[i]
+            return vec
+
+        def cc2():
+            for i in mesh.space_boundary_conditions.point_index_vector:
+                a_matrix[i, :] = 0
+                a_matrix[i, i] = 1
+            return
+
+        # --------------------------------- Solver ---------------------------------------------------------------------
+        cc2()
+        frames = [mesh.time_boundary_conditions.values_vector]
+        t_matrix = frames[-1]
+        for t in range(int(total_time / dt)):
+            b_matrix = dt * m_matrix.dot(q_matrix) + sparse.lil_matrix(m_matrix.dot(t_matrix)).transpose()
+            b_matrix = cc1(b_matrix)
+            t_matrix = linalg.spsolve(a_matrix, b_matrix)
+            frames.append(t_matrix)
+
+        return frames
 
     return 0
 
@@ -174,7 +227,6 @@ class Mesh:
     """
     x, y, ien = [], [], []
     size = 0
-    boundary_conditions = []
 
     class BoundaryConditions:
         """
@@ -184,7 +236,7 @@ class Mesh:
         values_vector = []
         type_of_condition_vector = []
 
-        def set_new_boundary_conditions(self, *vect_argm, point_index=0, values=0, type_of_boundary=0):
+        def set_new_boundary_conditions(self, *vect_argm, point_index=range(0), values=0, type_of_boundary=True):
             """
             Sets the boundary conditions for the mesh
             :param vect_argm: Vector(s) of boundary conditions
@@ -192,26 +244,48 @@ class Mesh:
             :param values: Values of the condition in each point
             :param type_of_boundary: Type True for Dirichlet and False for Neumann
             """
-            check_method_call(vect_argm, [point_index, values, type_of_boundary])
+            import numpy as np
+
+            # TODO: INCREASE CHECKS
+            if vect_argm:
+                check_method_call(vect_argm)
+            else:
+                check_method_call(point_index)
 
             try:
+                if isinstance(vect_argm[0], list):
+                    array = np.array(vect_argm[0])
+
                 if len(vect_argm[0][0]) == 3:
-                    self.point_index_vector = []
-                    self.values_vector = []
-                    self.type_of_condition_vector = []
+                    self.point_index_vector = array[:, 0]
+                    self.values_vector = array[:, 1]
+                    self.type_of_condition_vector = array[:, 2]
+                    return
 
-                    for item in vect_argm[0]:
-                        self.point_index_vector.append(item[0])
-                        self.values_vector.append(item[1])
-                        self.type_of_condition_vector.append(item[2])
+                if len(vect_argm[0][0]) == 2:
+                    self.point_index_vector = array[:, 0]
+                    self.values_vector = array[:, 1]
+                    self.type_of_condition_vector = [True] * len(self.point_index_vector)
+                    return
 
-            except TypeError:
+            except (TypeError, IndexError):
                 self.point_index_vector = point_index
-                self.values_vector = values
-                self.type_of_condition_vector = type_of_boundary
-
-            except IndexError:
-                raise RuntimeError("Unexpected Error")
+                if isinstance(values, list):
+                    if len(values) == len(point_index):
+                        self.values_vector = values
+                    else:
+                        raise ValueError("Incorrect vector sizes, there must be an equal number of points and point "
+                                         "types and definitions")
+                else:
+                    self.values_vector = [values] * len(point_index)
+                if isinstance(type_of_boundary, list):
+                    if len(type_of_boundary) == len(point_index):
+                        self.type_of_condition_vector = type_of_boundary
+                    else:
+                        raise ValueError("Incorrect vector sizes, there must be an equal number of points and point "
+                                         "types and definitions")
+                else:
+                    self.type_of_condition_vector = [type_of_boundary] * len(point_index)
 
     def __init__(self, default_boudary_conditions=True):
         """
@@ -221,7 +295,8 @@ class Mesh:
         """
         self.import_point_structure()
 
-        self.boundary_conditions = self.BoundaryConditions()
+        self.space_boundary_conditions = self.BoundaryConditions()
+        self.time_boundary_conditions = self.BoundaryConditions()
 
         # Default Boundary coditions declaration
         if default_boudary_conditions:
@@ -241,7 +316,8 @@ class Mesh:
                     # Boundaries are defined to be the lower and left sides with value 0
                     vector.append([index, 0, True])
 
-            self.boundary_conditions.set_new_boundary_conditions(vector)
+            self.space_boundary_conditions.set_new_boundary_conditions(vector)
+            self.time_boundary_conditions.set_new_boundary_conditions(point_index=range(self.size), values=0)
 
     def import_point_structure(self, *args):
         """
