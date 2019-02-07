11f20e407cc5331251da75df9895ff4b9b06a704
Lucas <encarvlucas@hotmail.com>
2019-02-07 10:27:54 -0200

    -Added method to save each frame so you can vizualize the movement of the particles faster.

--- Run.py
+++ Run.py
@@ -24,9 +24,9 @@ list(map(lambda _vect: malha.new_boundary_condition(_vect["name"], point_index=_
 poiseuille = True
 
 if poiseuille:
-    vel_x, vel_y = solve_poiseuille(malha, total_time=5.07, dt=.51, rho_coef=1e3, mu_coef=0.89e-3, save_each_frame=False)
-    malha.show_velocity_quiver(vel_x, vel_y)
-    malha.show_particle_movement(save=False)
+    vel_x, vel_y = solve_poiseuille(malha, total_time=5.07, dt=.51, rho_coef=1e3, mu_coef=0.89e-3, save_each_frame=True)
+    # malha.show_velocity_quiver(vel_x, vel_y)
+    # malha.show_particle_movement(save=False)
 
 else:
     Q = ComplexPointList([32, 39, 64, 67, 68, 70], 50.)
