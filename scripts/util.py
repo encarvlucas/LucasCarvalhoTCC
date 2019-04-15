--Util: -Fixed absolute error calculation, missing abs().
--Test Cases:
-Added several missing or incorrect labels and titles.

--Text:
-Made some progress, fixed errors based on Leandro Marques's notes.

Signed-off-by: Lucas <encarvlucas@hotmail.com>

scripts/util.py
@@ -239,7 +239,7 @@ def show_comparison(x_coordinates: np.ndarray, analytic_expression: callable, nu
     numeric_solution = np.array(numeric_solution if not isinstance(numeric_solution, dict) else
                                 numeric_solution[max(numeric_solution.keys())])
 
-    error_array = np.nan_to_num((numeric_solution - analytic_solution)/analytic_solution)
+    error_array = np.nan_to_num(abs(numeric_solution - analytic_solution)/analytic_solution)
     print("Mean Error: {0}\nStandard Error: {1}".format(np.mean(error_array), np.std(error_array)))
 
     if save_file_as is not None and isinstance(save_file_as, str):

test_setups/Run_validate_added_mass.py
@@ -59,5 +59,5 @@ y_vector = np.array(particle_a.position_history)[:-1, 0]
 # Show comparison graph
 TccLib.util.show_comparison(x_vector, analytic_expression, y_vector, numeric_label="Solução Numérica",
                             analytic_label="Solução Analítica", title="Força de Massa Virtual",
-                            x_label="Tempo(s)", y_label="Posição no Eixo X(m)",
+                            x_label="Tempo(s)", y_label="Posição no Eixo x(m)",
                             save_file_as="{0}_{1}_validation".format(mesh.name, force))