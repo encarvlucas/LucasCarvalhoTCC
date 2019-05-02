8c55853b28d5a1f418161520c688da24b30d206e
Lucas <encarvlucas@hotmail.com>
2019-05-02 16:19:42 -0300

    --Test Cases: -Successfully implemented Step and Obstacle cases.
    Signed-off-by: Lucas <encarvlucas@hotmail.com>

--- main_setups/Run_step_flow.py
+++ main_setups/Run_step_flow.py
@@ -4,8 +4,8 @@ import numpy as np
 
 # Define boundary conditions and parameters
 vel = 1.
-dt = 0.5
-total_time = 10.
+dt = 0.1
+total_time = 5.
 
 # Set liquid parameters or declare liquid
 # density = 1e3
@@ -23,10 +23,13 @@ sides_vertical = np.array([
     19, 20, 21, 22, 23, 24, 25, 26, 27,
     43, 44, 45, 46, 47, 48, 49, 50, 51,
 ])
-sides_horizontal = np.array([
-    3, 28, 29, 30, 31,
+sides_horizontal_right = np.array([
+    3, 28, 29, 30, 31
+])
+sides_horizontal_left = np.array([
     8, 52, 53, 54, 55
 ])
+sides = np.append(sides_vertical, np.append(sides_horizontal_right, sides_horizontal_left))
 
 # -------------------------------------------------- PSI ---------------------------------------------------------------
 # Define boundary conditions for psi
@@ -52,8 +55,9 @@ xy_indices, xy_values, xy_types = TccLib.build_boundary_conditions(mesh, boundar
 mesh.new_boundary_condition("psi", point_index=xy_indices, values=xy_values, type_of_boundary=xy_types)
 
 # Set boundary conditions for obstacle
-# mesh.new_boundary_condition("psi", point_index=sides_horizontal, values=mesh.y[sides_horizontal], type_of_boundary=True)
-mesh.new_boundary_condition("psi", point_index=sides_vertical, values=0., type_of_boundary=True)
+mesh.new_boundary_condition("psi", point_index=sides_horizontal_left, values=0., type_of_boundary=True)
+mesh.new_boundary_condition("psi", point_index=sides_horizontal_right, values=1., type_of_boundary=True)
+mesh.new_boundary_condition("psi", point_index=sides_vertical, values=0., type_of_boundary=False)
 
 # -------------------------------------------------- VEL X -------------------------------------------------------------
 # Define boundary conditions for vel_x
@@ -79,8 +83,7 @@ xy_indices, xy_values, xy_types = TccLib.build_boundary_conditions(mesh, boundar
 mesh.new_boundary_condition("vel_x", point_index=xy_indices, values=xy_values, type_of_boundary=xy_types)
 
 # Set boundary conditions for obstacle
-mesh.new_boundary_condition("vel_x", point_index=sides_horizontal, values=0., type_of_boundary=True)
-mesh.new_boundary_condition("vel_x", point_index=sides_vertical, values=0., type_of_boundary=True)
+mesh.new_boundary_condition("vel_x", point_index=sides, values=0., type_of_boundary=True)
 
 # -------------------------------------------------- VEL Y -------------------------------------------------------------
 # Define boundary conditions for vel_y
@@ -106,8 +109,7 @@ xy_indices, xy_values, xy_types = TccLib.build_boundary_conditions(mesh, boundar
 mesh.new_boundary_condition("vel_y", point_index=xy_indices, values=xy_values, type_of_boundary=xy_types)
 
 # Set boundary conditions for obstacle
-mesh.new_boundary_condition("vel_y", point_index=sides_horizontal, values=0., type_of_boundary=True)
-mesh.new_boundary_condition("vel_y", point_index=sides_vertical, values=0., type_of_boundary=True)
+mesh.new_boundary_condition("vel_y", point_index=sides, values=0., type_of_boundary=True)
 
 # Solve for FEM velocity field solution
 velocity_x, velocity_y = TccLib.solve_velocity_field(mesh, dt=dt, total_time=total_time, save_each_frame=True,
